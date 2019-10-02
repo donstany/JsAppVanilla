6b0f7769118f650c0a6e8df6cb2d01660631aa87
donstany <donstany79@gmail.com>
2019-10-02 15:02:05 +0300

    Refactoring first stage

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ import { MOCK } from "./MOCK_DATA.js";
     let keys = Object.keys(data[0]);
     let result ="";
 
+    function crateTag(tag, content){
+        return  `<${tag}>
+     ${Array.isArray(content) ? content.join("") : content}
+</${tag}>`;
+    }
+
     result +="<table>"
-    result +="  <thead>"
-    result +="     <tr>"
-    result += keys.map( key => `<th>${key}</th>`).join("");
-    result +="     </tr>"
-    result +="  </thead>"
+    result += crateTag(
+                "thead",
+                crateTag("tr", keys.map( key => crateTag("th", key)))
+                );
     result +=   "  <tbody>"
+
     result += data
-                .map( row  => `<tr>${ keys.map(cell => `<td>${row[cell]} </td>`)
-                                                .join("") }
-                               </tr>`
+                .map( row  => crateTag("tr", keys.map(cell => 
+                    crateTag("td", row[cell])))
                     )
-                .join("")
+
     result +="  </tbody>"
 
     result +="</table>"
  
-    document.getElementById("app").innerHTML = result;
+    document .getElementById("app").innerHTML = result;
 
 }(MOCK, document))
 // (MOCK, {getElementById: () => { } }))
