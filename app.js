143baa7a400c6d096d059ab73200888f15dc03ff
donstany <donstany79@gmail.com>
2019-10-03 14:00:35 +0300

    recognize content url, or list of items

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 import { MOCK } from "./MOCK_DATA.js";
 
 (function(data, document){
+
     let keys = Object.keys(data[0]);
+
     function crateTag(tag, content){
         return  `<${tag}>
      ${Array.isArray(content) ? content.join("") : content}
 </${tag}>`;
     }
 
-    const renderTable = crateTag.bind(undefined, "table");
+    function createSingleTag(tag, prop, val){
+        return `<${tag} ${prop}="${val}" />`;
+    }
+
+    const renderTable = crateTag.bind(undefined, "table"); // partial application techniques
     const renderThead = crateTag.bind(undefined, "thead");
     const renderTbody = crateTag.bind(undefined, "tbody");
     const renderTr = crateTag.bind(undefined, "tr");
     const renderTd = crateTag.bind(undefined, "td");
     const renderTh = crateTag.bind(undefined, "th");
+    const renderUl = crateTag.bind(undefined, "ul");
+    const renderLi = crateTag.bind(undefined, "li");
 
-   let result= renderTable(
+    function chooseContentType(map, defaultWrapperCallBack, type, content) {
+            if(typeof map[type] === "function"){
+               return defaultWrapperCallBack(map[type](content));
+            }
+            return defaultWrapperCallBack(content);
+    };
+    // render hook
+    const fieldsMap ={
+        avatar: x => createSingleTag("img", "src", x),
+        friends : list => renderUl(list.map(f => renderLi(`${f.first_name} ${f.last_name}`)))
+    }
+    const defaultTd = chooseContentType.bind(
+            undefined,
+            fieldsMap,
+            renderTd
+    );
+
+    let result = renderTable(
         renderThead(renderTr(keys.map( key => renderTh(key)))) 
         +
         renderTbody(
-           data.map( row  => renderTr( keys.map(cell => renderTd(row[cell]))))
-           )
+            data.map( row  => renderTr( keys.map(cell => defaultTd(cell,row[cell]))))
+                    )
     )
  
     document .getElementById("app").innerHTML = result;
 
-}(MOCK, document))
+}(MOCK.slice(0,20), document))
 // (MOCK, {getElementById: () => { } }))
 
 
